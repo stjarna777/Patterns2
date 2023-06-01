b184977c33e29251cb1782ab1a5a3eead522c5a0
anna_stjarna <artemyeva@gmail.com>
2023-06-01 08:49:01 +0300

    Added changes to test class

--- src/test/java/ru/netology/delivery/test/ChangeOfDateTest.java
+++ src/test/java/ru/netology/delivery/test/ChangeOfDateTest.java
@@ -1,6 +1,7 @@
 package ru.netology.delivery.test;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
